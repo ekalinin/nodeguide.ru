1e2082c1c9dc3c90cbe33664220400c5cb71948f
Eugene Kalinin <e.v.kalinin@gmail.com>
2011-05-23 00:00:46 +0400

    routing refactoring; universal doc hendler;

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-/**
- * Module dependencies.
- */
-
+// Modules
 var express = require('express');
 var path = require("path");
 var fs = require("fs"); 
 var os = require("os");
 
-var app = module.exports = express.createServer();
+// Documentation base path
+var doc_base = path.join(__dirname, 'build', 'json');
 
+// Documentation environment
+// (read from file that builded by sphinx)
 var env;
-var env_file = path.join(__dirname, 'build', 'json', 'globalcontext.json');
+fs.readFile(path.join(doc_base, 'globalcontext.json'), 'utf8', function (err, data) {
+    env = JSON.parse(data);
+});
 
-// Configuration
+// Application configuration
+var app = module.exports = express.createServer();
 app.configure( function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -24,45 +27,45 @@ app.configure( function() {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-// Read sphinx-env file
-fs.readFile(env_file, 'utf8', function (err, data) {
-    env = JSON.parse(data);
-});
-
 // Routes
 app.get('/', function (req, res) { res.redirect('/doc/'); });
+app.get(/doc$/, function (req, res) { res.redirect('/doc/'); });
+app.get(/doc\/([\w-\/]*)?$/, function (req, res, next) {
+    var url = req.params[0],
+        port = app.address().port,
+        // url point to file
+        doc_name = (url ? url.replace(/\/$/,'') : 'index') + '.fjson',
+        filename = path.join(doc_base, doc_name);
 
-app.get('/doc/', doc);
-app.get('/doc/:name', doc);
-app.get('/doc/:dir/:name', doc);
-function doc(req, res, next) {
-    var filename = path.join(__dirname, 'build', 'json', req.params.dir,
-                        (req.params.name ? req.params.name : 'index') +
-                        '.fjson');
-    var port = app.address().port;
+    console.log(' * filename #1: ' + filename);
 
     path.exists(filename, function(exists) {
-        // doc not found
-        if(!exists) {
-            res.render('layout', {
-                doc: {
-                    title:  'Документ не найден!',
-                    body:   'Документ не найден :( <br/> ' +
-                            'Попробуйте, пожалуйста, <a href="/"> сначала </a>.',
-                },
-                env: env,
-                port: port
+        // found
+        if (exists) {
+            fs.readFile(filename, 'utf8', function (err, data) {
+                res.render('layout', { doc: JSON.parse(data),
+                                        env: env, port: port});
             });
             return;
         }
-        // found
-        fs.readFile(filename, 'utf8', function (err, data) {
-            res.render('layout', { doc: JSON.parse(data), env: env,
-                            port: port});
+
+        // may be it was directory?
+        // add index.fjson
+        filename = path.join(doc_base, url, 'index.fjson');
+        console.log(' * filename #2: ' + filename);
+        path.exists(filename, function (exists) {
+            if (!exists) {
+                res.send(404);
+                return;
+            }
+
+            fs.readFile(filename, 'utf8', function (err, data) {
+                res.render('layout', { doc: JSON.parse(data),
+                                        env: env, port: port});
+            });
         });
     });
-}
-
+});
 
 // Only listen on $ node app.js
 if (!module.parent) {
